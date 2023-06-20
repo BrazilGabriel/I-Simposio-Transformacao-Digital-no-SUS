9bb2521e9ffb885ed58fec76d681b32565deb37f
unknown <gabriel.br1994@gmail.comm>
2023-06-20 11:33:26 -0400

    fist commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css'
+import '../styles/animation.css'
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/footer.css'
+
+
+
+import '../styles/utils.css'
+
+import animationController from './utils/animationController.js';
+
+
+
+animationController();
---
+++ src/utils/animationController.js
@@ -0,0 +1,19 @@
+const animationController = () => {
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting) {
+        entry.target.classList.add('animated');
+      } else {
+        entry.target.classList.remove('animated');
+      };
+      
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.animate');
+  hiddenElements.forEach((el) => observer.observe(el));
+  
+}
+export default animationController;
