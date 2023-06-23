c6b518123b5942e44aa05b8384de6bdaee7f647b
unknown <gabriel.br1994@gmail.comm>
2023-06-22 22:05:56 -0400

    logo e melhorias

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -1,23 +1,25 @@
 const mobileNav = () => {
     const headerBtn = document.querySelector('.header__bars');
+    const headerImg = document.querySelector('.header__icon');
     const mobileNav = document.querySelector('.mobile-nav');
     const mobileLinks = document.querySelectorAll('.mobile-nav__link');
 
     //State
     let isMobileNavOpen = false;
 
-    headerBtn.addEventListener('click', () => {
+    headerBtn.addEventListener('change', () => {
         isMobileNavOpen = !isMobileNavOpen;
         if (isMobileNavOpen) {
             mobileNav.style.display = 'flex';
+            headerImg.style.visibility = 'hidden';
             document.body.style.position = 'relative';
-            
             document.body.style.overflowY = 'hidden';
 
         } else {
             mobileNav.style.display = 'none';
             document.body.style.position = 'absolute'
             document.body.style.overflowY = 'auto';
+            headerImg.style.visibility = 'visible';
 
         }
     });
@@ -27,6 +29,8 @@ const mobileNav = () => {
             isMobileNavOpen = false;
             mobileNav.style.display = 'none';
             document.body.style.overflowY = 'auto';
+            headerImg.style.visibility = 'visible';
+            headerBtn.checked = false;
         })
     });
 
@@ -35,6 +39,8 @@ const mobileNav = () => {
             isMobileNavOpen = false;
             mobileNav.style.display = 'none';
             document.body.style.overflowY = 'auto';
+            headerImg.style.visibility = 'visible';
+            headerBtn.checked = false;
         }
       }
       
