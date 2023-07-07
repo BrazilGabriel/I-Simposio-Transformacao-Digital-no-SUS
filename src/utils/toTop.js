514c0b11e3899f0ca06d769ffbb8285902b2bf8a
unknown <gabriel.br1994@gmail.comm>
2023-07-07 18:30:57 -0400

    sticky menu

--- src/utils/toTop.js
+++ src/utils/toTop.js
@@ -1,13 +1,24 @@
 const backToTop = () => {
+    const navbar = document.getElementById('navbar');
+    const menu = document.getElementById('menu');
+    const toTop = document.querySelector(".to-top");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            toTop.classList.add("active");
+        } else {
+            toTop.classList.remove("active");
+        }
 
-const toTop = document.querySelector(".to-top");
-window.addEventListener("scroll", () => {
-    if(window.scrollY > 100){
-        toTop.classList.add("active");
-    } else {
-        toTop.classList.remove("active");
-    }
-})
+        if(window.scrollY >= 10){
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    })
+
+
+
+
 
 }
 export default backToTop;
