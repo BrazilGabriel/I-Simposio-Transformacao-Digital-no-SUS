96a5c02bfba6f8a646deeab9f1bf5247c8ce6747
unknown <gabriel.br1994@gmail.comm>
2023-07-12 08:21:04 -0400

    logo no menu sticky

--- src/utils/toTop.js
+++ src/utils/toTop.js
@@ -1,6 +1,6 @@
 const backToTop = () => {
     const navbar = document.getElementById('navbar');
-    const menu = document.getElementById('menu');
+    const logo = document.getElementById('logo');
     const toTop = document.querySelector(".to-top");
     window.addEventListener("scroll", () => {
         if (window.scrollY > 100) {
@@ -11,8 +11,12 @@ const backToTop = () => {
 
         if(window.scrollY >= 700){
             navbar.classList.add("sticky");
+            logo.classList.add("sticky");
+
         } else {
             navbar.classList.remove("sticky");
+            logo.classList.remove("sticky");
+
         }
     })
 
